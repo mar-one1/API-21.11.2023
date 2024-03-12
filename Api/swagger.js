cefd44ba84f1f4028b75b8a55cef824e48c04fd9
marwane <marwane.rays@gmail.com>
2024-03-12 14:30:52 +0000

    make swagger implementation

---
+++ Api/swagger.js
@@ -0,0 +1,19 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Your API Documentation',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./Api/Model/*.js'], // Path to your API route files
+  };
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
